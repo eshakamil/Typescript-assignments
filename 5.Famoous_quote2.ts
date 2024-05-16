6e1e1868a5ac06040fb7d1db7bfe12035900c5ae
eshakamil <eshyykhan729@gmail.com>
2024-05-16 09:38:33 -0700

    Add files via upload

---
+++ 5.Famoous_quote2.ts
@@ -0,0 +1,3 @@
+let famous_person :string = "ALBERT EINSTIEN";
+let message :string = "'A person who never made a mistake never tried any thing new'."
+console.log(`${famous_person} once said, ${message}`);
