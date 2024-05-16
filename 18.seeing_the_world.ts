6b94e55f439b17caa2b0c47f57ffd179b1c9766a
eshakamil <eshyykhan729@gmail.com>
2024-05-16 13:46:16 -0700

    Add files via upload

---
+++ 18.seeing_the_world.ts
@@ -0,0 +1,36 @@
+//Store the location in the array. Make sure the array is not a alphabatical order 
+
+let places :string[] = ['Naran','Kagan','Swat','Kalam','Malam Jabba'];
+
+//Print your array in its original order
+console.log('Original ' + places);
+
+console.log('Copy ' + [...places].sort());
+
+//Show that your array is still in  its original order by printing it.
+
+console.log('Original ' + places);
+
+//Print your array in reverse alphabetical order with out changing the orginal of the order list.
+
+console.log('copy ' + [...places].sort().reverse());
+
+//show that your array is still in its orginal order by printing it again.
+
+console.log('Original ' + places);
+
+//reverse the order of your list. Print the array to show that its order has changed.
+
+console.log('Original ' + places);
+
+//Reverse the order of your list again. print the list to show it's back to its orginal order.
+console.log('Original ' + places);
+
+//Sort your array so it's stored in alphabatical order. Print the array to show that its ordered has been changed.
+console.log('Original ' + places);
+
+//Sort to change your array so it's stored in reverse alphabatical order. Print the list to show that its order has changed.
+
+
+console.log('Original ' + places);
+
