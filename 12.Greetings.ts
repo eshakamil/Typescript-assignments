f0613a5309176df9f10b8d3559665a01b5b5530b
eshakamil <eshyykhan729@gmail.com>
2024-05-16 13:31:41 -0700

    Add files via upload

---
+++ 12.Greetings.ts
@@ -0,0 +1,8 @@
+let names :string[] = ['Osaid','Kamil','Esha','Akasha'] ;
+
+
+const message :string = 'Welcome to type script class: '
+
+for(let i=0; i<names.length; i++){
+    console.log(message + names[i]) ;
+}
