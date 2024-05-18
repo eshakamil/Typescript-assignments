2f94da8d0878a43afa94845fc09585f8087b84d0
eshakamil <eshyykhan729@gmail.com>
2024-05-18 09:34:52 -0700

    Add files via upload

---
+++ 22.Intentional_error.ts
@@ -0,0 +1,11 @@
+//if you haven't received an array index error in one of your programs yet,try to make one happen
+
+//creating array
+let newArray :string []=['will smith', 'james bond', 'tom cruise'];
+
+
+//accessing array by index
+console.log(newArray[5]);
+
+//correct the error before closing the program
+console.log(newArray);
