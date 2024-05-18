cdb4094dffa21dfda13646c52d7e4dab0db8d09f
eshakamil <eshyykhan729@gmail.com>
2024-05-18 09:58:22 -0700

    Add files via upload

---
+++ 31.no_user.ts
@@ -0,0 +1,9 @@
+let userNames:string[]=['admin','junaid','zeeshan','kamran','daniyal']
+if(userNames.length === 0){
+    console.log('we need to find some users!');
+    
+}else{
+    userNames = [];
+    console.log('all usernames has been removed' + userNames.length);
+    
+}
