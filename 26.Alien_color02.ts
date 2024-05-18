e5e37f72b7046471f836fb2d1defe20e4546f2b3
eshakamil <eshyykhan729@gmail.com>
2024-05-18 09:44:08 -0700

    Add files via upload

---
+++ 26.Alien_color02.ts
@@ -0,0 +1,21 @@
+//alien color 02:choose a color for an alienas you did in ex #25
+
+let alienColor: string = "green";
+
+//write an if else chain
+//if the alien color is green.
+//print a statement that the player just earned 5 points for shooting the alien.
+if(alienColor === "green"){
+console.log("player just earned 5 points for shooting the alien.");
+}else{
+    console.log("player just earn 10 points.");
+    
+}
+//if the alien color isn't green,print a statement that the player just earned 10 points.
+alienColor = "yellow";
+
+if (alienColor === "green"){
+    console.log("player just earned 5 ppoints for shooting the alien.");
+}else{
+    console.log("player just earn 1o points.")
+}
