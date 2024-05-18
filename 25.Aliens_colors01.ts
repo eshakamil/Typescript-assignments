a72505b6c45b2038b82222e6e09025fcd9cb5141
eshakamil <eshyykhan729@gmail.com>
2024-05-18 09:42:16 -0700

    Add files via upload

---
+++ 25.Aliens_colors01.ts
@@ -0,0 +1,18 @@
+//alien colors 01 : imagine an alien was just shotdown in a game . 
+//Create a variable called alien_color and assign it a value of green, yellow, or red.
+
+let alien_color :string = 'green';
+
+//write an if statement to test wheteher the alien's color is green. 
+//If it is,print a message that the player just earned 5 points
+if (alien_color === 'green'){
+    console.log("player just earned 5 points !");
+}
+
+// write one version of this program that passes the if test and another that fails
+
+alien_color = 'yellow' ;
+//(the version that fails will have no output.) 
+if (alien_color === 'green'){
+    console.log("player just earned 5 points !");
+}
