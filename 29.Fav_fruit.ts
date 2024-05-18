388cce18df00734ebae67b953516c24ebd82db1b
eshakamil <eshyykhan729@gmail.com>
2024-05-18 09:53:00 -0700

    Add files via upload

---
+++ 29.Fav_fruit.ts
@@ -0,0 +1,22 @@
+
+let favFruit : string[]=['apple','mango','banana'];
+
+if(favFruit.includes('apple')){
+    console.log('I really like apple!')
+
+}
+if(favFruit.includes('banana')){
+    console.log('I really like banana!')
+}
+
+if(favFruit.includes('mango')){
+    console.log('I really like mango!')
+}
+
+if(favFruit.includes('strawberries')){
+    console.log('I really like strawberries!')
+}
+
+if(favFruit.includes('orange')){
+    console.log('I really like orange!')
+}
