6ce504670f458ff1180929062e6c3fe960d5be0b
eshakamil <eshyykhan729@gmail.com>
2024-05-18 10:13:26 -0700

    Add files via upload

---
+++ 37.large_shirt.ts
@@ -0,0 +1,9 @@
+
+function make_shirt(size:string ='Large', text:string ='I Love Typescript.'){
+    console.log(`creating a ${size}shirt with the mesage: ${text}`)
+}
+make_shirt();
+
+make_shirt('Medium');
+
+make_shirt('small','I Love Python');
