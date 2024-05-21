aad2bcef199a17253f117c6b5253a662e44ecdb3
eshakamil <eshyykhan729@gmail.com>
2024-05-21 12:28:35 -0700

    Add files via upload

---
+++ 44.sandwiches.ts
@@ -0,0 +1,14 @@
+
+function makeSandwich (item: string[]){
+    console.log("\n making your sandwich with:");
+
+    item.forEach(element => console.log("- " +element));
+    console.log('Enjoy your sandwich!\n');
+
+}
+
+makeSandwich(['Ham','Cheese','lettuce']);
+
+makeSandwich(['Turkey','Bacon']);
+
+makeSandwich(['Peanut butter','Jelly',  ]);
