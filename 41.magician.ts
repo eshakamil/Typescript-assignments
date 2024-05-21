9d4ac43d45403a40024389a7c22c6551d293a746
eshakamil <eshyykhan729@gmail.com>
2024-05-21 12:25:54 -0700

    Add files via upload

---
+++ 41.magician.ts
@@ -0,0 +1,10 @@
+let magician : string[]=['Harry Potter','Hermione Granger','Ron Weasley','Albus Dumbledore'],
+
+function show_magician(magicians:string[]){
+
+    magicians.forEach(element => {
+        console.log(element);
+    });
+}
+
+show_magician(magician);
